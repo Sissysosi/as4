30d6318e5c422833eb2eea746a49edad371134ab
Sissysosi <82708313+Sissysosi@users.noreply.github.com>
2022-05-13 14:42:40 +0800

    Update login.page.ts

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,33 +1,88 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { LoadingController, ModalController } from '@ionic/angular';
-import { modalController } from '@ionic/core';
-import { loadingController } from '@ionic/core';
-
+import { Router } from "@angular/router";
+import { IonicAuthService } from '../ionic-auth.service';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
+
 export class LoginPage implements OnInit {
+
+  userForm: FormGroup;
+  successMsg: string = '';
+  errorMsg: string = '';
   
+
+  error_msg = {
+    'email': [
+      { 
+        type: 'required', 
+        message: 'Provide email.' 
+      },
+      { 
+        type: 'pattern', 
+        message: 'Email is not valid.' 
+      }
+    ],
+    'username': [
+      { 
+        type: 'required', 
+        message: 'provide username.' 
+      }
+     
+    ],
+    'password': [
+      { 
+        type: 'required', 
+        message: 'Password is required.' 
+      },
+      { 
+        type: 'minlength', 
+        message: 'Password length should be 6 characters long.' 
+      }
+    ]
+  };
+
   constructor(
-    public router: Router,
-    public modalCtrl: ModalController,
-    public loadCtrl : LoadingController,
+    private router: Router,
+    private ionicAuthService: IonicAuthService,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit() {
+    this.userForm = this.fb.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      username: new FormControl('', Validators.compose([
+        Validators.required,
+        
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(6),
+        Validators.required
+      ])),
+    });
   }
-  async login(){
-    this.router.navigateByUrl('home');
+
+  signIn(value) {
+    this.ionicAuthService.signinUser(value)
+      .then((response) => {
+        console.log(response)
+        this.errorMsg = "";
+        this.router.navigateByUrl('home');
+      }, error => {
+        this.errorMsg = error.message;
+        this.successMsg = "";
+      })
   }
-  async dismiss(){
-  
-      this.router.navigateByUrl('welcome');
-    
+
+  goToSignup() {
+    this.router.navigateByUrl('register');
   }
- 
 
 }

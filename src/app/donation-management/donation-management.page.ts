98f128f4b7913f667a7b0d2d602b420b1d8ebe66
Adam Danish <81909121+DNSH26@users.noreply.github.com>
2022-05-14 00:22:24 +0800

    Add files via upload

---
+++ src/app/donation-management/donation-management.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-donation-management',
+  templateUrl: './donation-management.page.html',
+  styleUrls: ['./donation-management.page.scss'],
+})
+export class DonationManagementPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

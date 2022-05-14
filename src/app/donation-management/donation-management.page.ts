90bbf24c37ba77afbf5b1322705f2b0c28a8d77e
Adam Danish <81909121+DNSH26@users.noreply.github.com>
2022-05-14 20:21:20 +0800

    Add files via upload

--- src/app/donation-management/donation-management.page.ts
+++ src/app/donation-management/donation-management.page.ts
@@ -7,7 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonationManagementPage implements OnInit {
 
-  constructor() { }
+  capturedSnapURL: string;
+
+  cameraOptions: CameraOptions = {
+    quality: 20,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  };
+
+  constructor(private camera: Camera) { }
+
+  takeSnap() {
+    this.camera.getPicture(this.cameraOptions).then((imageData) => {
+      // this.camera.DestinationType.FILE_URI gives file URI saved in local
+      // this.camera.DestinationType.DATA_URL gives base64 URI
+
+      const base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.capturedSnapURL = base64Image;
+    }, (err) => {
+
+      console.log(err);
+      // Handle error
+    });
+  }
 
   ngOnInit() {
   }

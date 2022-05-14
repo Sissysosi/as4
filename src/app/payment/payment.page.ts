a5d93d347ee779c9e0456fc4e62e70d21b44da00
farhan <20ftt1451@student.pb.edu.bn>
2022-05-14 14:23:09 +0800

    modified payment page

--- src/app/payment/payment.page.ts
+++ src/app/payment/payment.page.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.page.html',
   styleUrls: ['./payment.page.scss'],
 })
+
 export class PaymentPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertCtrl: AlertController,
+              private navCtrl: NavController) { }
 
   ngOnInit() {
   }
+
+  async showPrompt() {
+    const alert = await this.alertCtrl.create({
+      header: "Confirm",
+      message: "Are You Confirm?",
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          handler: () => {
+            console.log("Cancel the form");
+            this.navCtrl.navigateRoot('/payment'); 
+          },
+        },
+        {
+          text: "Okay",
+          handler: () => {
+            console.log("Submit the form");
+            this.navCtrl.navigateRoot('/home'); 
+          },
+        },
+      ],
+    });
+  
+    await alert.present();
+  }
+
+
 
 }

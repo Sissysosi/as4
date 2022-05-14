74f44987373b6f080118c68ef7084917b7ee660b
Adam Danish <81909121+DNSH26@users.noreply.github.com>
2022-05-14 20:13:07 +0800

    Edited files

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,21 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'as4mwe',
-  appName: 'donate2nation',
+  appId: 'io.ionic.starter',
+  appName: 'donate',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  cordova: {
+    preferences: {
+      ScrollEnabled: 'false',
+      BackupWebStorage: 'none',
+      SplashMaintainAspectRatio: 'true',
+      FadeSplashScreenDuration: '300',
+      SplashShowOnlyFirstTime: 'false',
+      SplashScreen: 'screen',
+      SplashScreenDelay: '3000'
+    }
+  }
 };
 
 export default config;
